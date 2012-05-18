e3b7f27e7bf525f2e8830ea1c28eaef78b6417f8
openartskills <openartskills@gmail.com>
2012-05-18 21:13:33 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oas::Application.config.secret_token = 'a1dd86ae6387618467143f47b97a2adab0c0a2a523f89c785e4bb4550eb70be3323707133ad0d8ee27be52ec01afd535d1fd31e0e7a35787d98b5908cfacfbfb'
